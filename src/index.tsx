55c62df5e17169f73e45348b4f4d189d47aa8c63
Steeven <steeven.kang@legratin.io>
2023-05-12 13:48:39 +0200

    Remove

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
-const PaymentRequestsSchema = require("./models/paymentRequestSchema");
 import express from "express";
+import mongoose from "mongoose";
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const cors = require("cors");
+
 const PORT = 5000;
 const mongoURL =
   "mongodb+srv://admin:123Admin456@paymentdbtest.qubrpxb.mongodb.net/paymentRequests";
 const app = express();
-const cors = require("cors");
-import mongoose from "mongoose";
 app.use(cors());
 app.use(express.json());
 
@@ -39,7 +41,7 @@ app.post("/DB", async (req: express.Request, res: express.Response) => {
       expirationDate: req.body.expirationDate,
       name: req.body.name,
     }); // Assign req.body values to the document
-    //console.log(newPaymentRequest);
+    // console.log(newPaymentRequest);
     newPaymentRequest
       .save()
       .then((result: any) => {
@@ -81,15 +83,16 @@ app.put(
       if (!account2Update) {
         return res.status(404).json({ message: "Account not found" });
       }
-      if ((account2Update.amount -= req.body.amount) >= 0) {
+      const UpdatedAmount: number = account2Update.amount - req.body.amount;
+      if (UpdatedAmount >= 0) {
         await account2Update.save();
       } else {
         return res.status(400).json({ message: "Insufficient Balance" });
       }
-      res.status(200).json({ message: "Account Balance Updated" });
       console.log("Account Balance Updated");
+      return res.status(200).json({ message: "Account Balance Updated" });
     } catch (error: any) {
-      res.status(500).json({ message: error.message });
+      return res.status(500).json({ message: error.message });
     }
   }
 );
